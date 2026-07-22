81051f83b844492eafd1e679cf78e4019a13158f
Javier Rodriguez <javier.dev2003@gmail.com>
2026-07-22 14:38:33 -0400

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://foodtruck-front-alpha.vercel.app',
+    ],
     credentials: true,
   });
 
